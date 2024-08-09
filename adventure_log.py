6098418402c56455aec350b921891882c8302663
Malikd96 <96malikdavis@gmail.com>
2024-08-09 16:02:33 -0400

    Updated Py File

--- adventure_log.py
+++ adventure_log.py
@@ -1,5 +1,5 @@
 def greet_coding_temple():
-    print("Ahoy, Coding Temple Navigator! Are you ready for today's adventure?")
+    print("Hello, Coding Temple Navigator!")
 
     if __name__ - - "__main__":
         greet_coding_temple()
